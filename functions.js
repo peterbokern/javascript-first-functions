7039f2009a2e5250bc4d229acdc7eccb1a760130
peterbokern <peterbokern@gmail.com>
2024-06-27 19:06:03 +0200

    Uitwerkingen compleet

--- functions.js
+++ functions.js
@@ -14,20 +14,34 @@ console.log(greeting);
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
 
+function getSchool() {
+  console.log("Novi Hogeschool");
+}
 
+getSchool();
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
 
+function giveCompliment() {
+  console.log("Lekker bezig met die functies, Jan!");
+}
 
+giveCompliment();
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
 
-
+const getGrades = () => [8,8.5,6,7];
+const grades = getGrades();
+console.log(grades);
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+const getDetails = () => ({firstName : "Jan", lastName : "Janssen"});
+const details = getDetails();
+console.log(details);
